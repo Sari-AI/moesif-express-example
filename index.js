744b7c8d515fe39b898b46d8b56056449c8c829c
Raymel Francisco <franciscoraymel@gmail.com>
2024-08-14 19:40:53 +0800

    updates to user and company identification

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ var bodyParser = require('body-parser');
 var app = express();
 var superagent = require('superagent');
 var _ = require('lodash');
+var Joi = require('joi');
+require('dotenv').config();
 
 var httpProxy = require('http-proxy');
 
 var moesif = require('moesif-nodejs');
+
+const { expressjwt } = require("express-jwt");
 
 
 var port = process.env.PORT || 5050
@@ -21,6 +25,9 @@ var moesifOptions = {
   debug: true,
 
   identifyUser: function (req, res) {
+    if (req.auth) {
+      return req.auth.id;
+    }
     if (req.user) {
       return req.user.id;
     }
@@ -79,6 +86,59 @@ moesifOptions.maskContent = function (event) {
 
 var moesifMiddleware = moesif(moesifOptions);
 
+const userSchema = Joi.object({
+  userId: Joi.string().required(),
+  companyId: Joi.string(),
+  metadata: Joi.object({
+    email: Joi.string().email().required(),
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    title: Joi.string(),
+    salesInfo: Joi.object({
+      stage: Joi.string(),
+      lifetimeValue: Joi.number(),
+      accountOwner: Joi.string().email()
+    })
+  }).required()
+});
+
+// Middleware for user validation
+const validateUser = (req, res, next) => {
+  console.log("validateUser")
+  const { error } = userSchema.validate(req.body);
+  console.log("error", error)
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
+
+const companySchema = Joi.object({
+  companyId: Joi.string().required(),
+  companyDomain: Joi.string(),
+  metadata: Joi.object({
+    orgName: Joi.string().required(),
+    planName: Joi.string().required(),
+    dealStage: Joi.string(),
+    mrr: Joi.number(),
+    demographics: Joi.object({
+      alexaRanking: Joi.number(),
+      employeeCount: Joi.number(),
+    })
+  }).required()
+});
+
+// Middleware for user validation
+const validateCompany = (req, res, next) => {
+  console.log("validateCompany")
+  const { error } = companySchema.validate(req.body);
+  console.log("error", error)
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
+
 app.use(moesifMiddleware);
 // moesifMiddleware.startCaptureOutgoing();
 
@@ -93,7 +153,7 @@ app.post('/multipart', function (req, res) {
   res.send('received');
 });
 
-app.get('/large-string-response', function(req, res) {
+app.get('/large-string-response', function (req, res) {
   var really_long_string = (new Array(10001)).join("x");
   res.send(really_long_string);
 });
@@ -103,33 +163,122 @@ app.get('/large-string-response', function(req, res) {
 var router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: true }));
-router.use(express.json({limit: '50mb', extended: true}));
-router.use(bodyParser.text({type: 'text/plain'}))
+router.use(express.json({ limit: '50mb', extended: true }));
+router.use(bodyParser.text({ type: 'text/plain' }))
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   console.log('req body in customer api');
   console.log(req.body);
-  res.json({ message: 'first json api'});
+  res.json({ message: 'first json api' });
 });
 
-router.post('/large', function(req, res) {
+router.post('/large', function (req, res) {
   console.log('req body in customer api');
   console.log(req.body);
-  res.json({ message: 'post successful'})
+  res.json({ message: 'post successful' })
 });
 
-router.get('/large-object-response', function(req, res) {
+// ADD COMPANY
+router.post('/update-company', validateCompany, function (req, res) {
+  console.log('req body in customer api');
+  console.log(req.body);
+
+  var company = req.body;
+
+  // Only companyId is required.
+  // Campaign object is optional, but useful if you want to track ROI of acquisition channels
+  // See https://www.moesif.com/docs/api#update-a-company for campaign schema
+  // metadata can be any custom object
+  // const company = {
+  //   companyId: '67890',
+  //   companyDomain: 'acmeinc.com', // If domain is set, Moesif will enrich your profiles with publicly available info
+  //   metadata: {
+  //     orgName: 'Acme, Inc',
+  //     planName: 'Free Plan',
+  //     dealStage: 'Lead',
+  //     mrr: 24000,
+  //     demographics: {
+  //       alexaRanking: 500000,
+  //       employeeCount: 47,
+  //     },
+  //   },
+  // };
+
+  const callback = () => { }
+
+  moesifMiddleware.updateCompany(company, callback);
+
+  res.json({ message: 'post successful' })
+});
+
+// ADD USER
+router.post('/update-user', validateUser, function (req, res) {
+  console.log('req body in user api');
+  console.log(req.body);
+
+  var user = req.body;
+
+  // Only userId is required.
+  // Campaign object is optional, but useful if you want to track ROI of acquisition channels
+  // See https://www.moesif.com/docs/api#users for campaign schema
+  // metadata can be any custom object
+  // var user = {
+  //   userId: '12345',
+  //   companyId: '67890', // If set, associate user with a company object
+  //   metadata: {
+  //     email: 'john@acmeinc.com',
+  //     firstName: 'John',
+  //     lastName: 'Doe',
+  //     title: 'Software Engineer',
+  //     salesInfo: {
+  //       stage: 'Customer',
+  //       lifetimeValue: 24000,
+  //       accountOwner: 'mary@contoso.com'
+  //     }
+  //   }
+  // };
+
+  const callback = () => { }
+
+  moesifMiddleware.updateUser(user, callback);
+
+  res.json({ message: 'post successful' })
+});
+
+router.post('/identify-user', expressjwt({ secret: 'shhhhhhared-secret', algorithms: ['HS256'] }), function (req, res) {
+  console.log('req body in user api');
+  console.log('req.body', req.body);
+  console.log('req.auth', req.auth);
+
+  // ONLY WORKS ON CLIENT SIDE
+  // moesif.identifyUser("12345");
+
+  res.json({ message: 'post successful' })
+});
+
+router.post('/add-credits-to-company', expressjwt({ secret: 'shhhhhhared-secret', algorithms: ['HS256'] }), function (req, res) {
+  console.log('req body in user api');
+  console.log('req.body', req.body);
+  console.log('req.auth', req.auth);
+
+  // ONLY WORKS ON CLIENT SIDE
+  // moesif.identifyUser("12345");
+
+  res.json({ message: 'post successful' })
+});
+
+router.get('/large-object-response', function (req, res) {
   var reallyBigArray = (new Array(10001)).fill('hi');
   res.json(reallyBigArray);
 });
 
-router.get('/outgoing/posts', function(req, res) {
+router.get('/outgoing/posts', function (req, res) {
   console.log('outgoing is called');
   superagent.get('https://jsonplaceholder.typicode.com/todos/2').then(function (response) {
     console.log('back from outoging');
     console.log(response.body);
     res.json({ fromTypicode: response.body });
-  }).catch(function(err) {
+  }).catch(function (err) {
     res.status(500).json(err);
   });
 });
@@ -208,6 +357,7 @@ app.use('/proxy', proxyRoute);
 
 app.use('/gov', governanceRoutes);
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Example app is listening on port ' + port);
 });
+
